69912bb491079918da37c9661ef284ca0618b50c
dreymotoh <striclyspoken@gmail.com>
2024-02-21 19:05:45 -0500

    lots of figuring out! adjusting, fixing bugs, removed favicon and logo, added tjs, light mode // dark mode preference in stylesheet, made components and icons directory, added plugin to fix icons bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [icon()],
+});

bec86a18661b72b18dee0bfc53533a92a2deea6f
dreymotoh <striclyspoken@gmail.com>
2024-04-03 16:57:37 -0400

    feat: about page styling, cleaning up of unnecessary stuff

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+    colors: {
+      mainRed: '#EE3124',
+      darkGrey: '#9B9BA9',
+      lightGrey: '#D3D3E3',
+      black: '#000',
+      white: '#fffff'
+    }
+  },
+  plugins: []
+};
